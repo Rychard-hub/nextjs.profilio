39aa14f7d861f3b8fae0160aeb1f2beaaeb244e3
Rychard-hub <69556162+Rychard-hub@users.noreply.github.com>
2022-10-16 20:36:14 +0300

    Initial commit

---
+++ pages/posts/first-post.js
@@ -0,0 +1,26 @@
+import Link from 'next/link';
+import Head from 'next/head';
+import Layout from '../../components/layout';
+
+export default function FirstPost() {
+    return (
+      <Layout>
+    
+      <Head>
+        <title>First Post real</title>
+      </Head>
+        
+       <header> 
+      <div>
+        <p>First Post is ReAl Deal</p>
+
+
+        </div>
+
+      </header>
+  
+    <h3>Back to <Link href="/"><a>At HOME</a></Link></h3>
+    
+  </Layout>
+    );
+}
