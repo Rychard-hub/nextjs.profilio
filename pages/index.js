f7ecdb4f194e24f44f21809c89bb281b4312863f
Rychard-hub <69556162+Rychard-hub@users.noreply.github.com>
2022-11-06 18:52:38 +0200

    index.js  './posts/picture'

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Head from 'next/head';
 import Image from 'next/image';
 import styles from '../styles/Home.module.css';
 import Link from 'next/link';
-import Picture from '../posts/picture';
+import Picture from './posts/picture';
 
 
 export default function Home() {
