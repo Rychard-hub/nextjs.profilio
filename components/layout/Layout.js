efc4dcfe24541fa7d0528de13fabe332fcf8f94b
Rychard-hub <69556162+Rychard-hub@users.noreply.github.com>
2023-03-01 18:27:14 +0200

    delete <div>

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -3,7 +3,7 @@ import classes from './Layout.module.css'
 function Layout(props) {
   return (
     <div>
-      <div></div>
+      
       <main className={classes.main}>{props.children}</main>
     </div>
   )
