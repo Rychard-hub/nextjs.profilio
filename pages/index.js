e294be97c002c056abbdebcc7994ccf625f7b481
Rychard-hub <69556162+Rychard-hub@users.noreply.github.com>
2023-03-02 17:14:44 +0200

    import /styles/global.css, title font-size: 2rem

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import styles from '../styles/Home.module.css';
+import styles from '../styles/global.css';
 import Link from 'next/link';
 import ProfilItem from '../components/profilio/ProfilItem';
 
