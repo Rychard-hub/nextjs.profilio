a7e339a4d13090245dbb727459269de967a4afda
Rychard-hub <69556162+Rychard-hub@users.noreply.github.com>
2023-04-28 21:53:26 +0300

    in exercises  no nothing CSS

--- pages/exercises/index.js
+++ pages/exercises/index.js
@@ -1,12 +1,12 @@
 import Link from 'next/link';
-import styles from '../styles/globals.css';
+
 
 const Exercises = () => {
     return (
         <div>
             <h2>There is example exercise</h2>
            
-            <h1 className={styles.title}>
+            <h1 >
               Go to<Link href="/">first page</Link>
             </h1>
         </div>
